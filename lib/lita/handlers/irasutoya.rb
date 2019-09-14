82ca02d21ad7191ccc6109f502a631f557c611e8
Yuji Ueki <unhappychoice@gmail.com>
2019-09-15 01:48:01 +0900

    :+1: Use irasutoya gem

--- lib/lita/handlers/irasutoya.rb
+++ lib/lita/handlers/irasutoya.rb
@@ -1,5 +1,5 @@
-require 'open-uri'
-require 'nokogiri'
+require 'lita'
+require 'irasutoya'
 
 module Lita
   module Handlers
@@ -11,48 +11,28 @@ module Lita
       )
 
       def irasutoya(bot)
-        url = get_random_url
-        document = get_page_and_parse(url)
-        image_url = document.css('.entry').search('img').attribute('src').value
-
+        irasuto = ::Irasutoya.random
+        p irasuto
         case robot.config.robot.adapter
         when :slack
           send_attachement(
             target: bot.room,
-            url: url,
-            title: document.css('.post').css('.title').search('h2').text,
-            body: document.css('.entry').css('.separator')[1].text,
-            image_url: image_url.chars.first == '/' ? 'http:' + image_url : image_url
+            url: irasuto.url,
+            title: irasuto.title,
+            body: irasuto.description,
+            image_url: irasuto.image_url
           )
         else
-          bot.reply document.css('.post').css('.title').search('h2').text
-          bot.reply document.css('.entry').css('.separator')[1].text
-          bot.reply image_url
+          bot.reply irasuto.url
+          bot.reply irasuto.title
+          bot.reply irasuto.description
+          bot.reply irasuto.image_url
         end
       end
 
-      private def get_random_url
-        luck = Random.rand(22208)
-        jsonp = Net::HTTP.get('www.irasutoya.com', "/feeds/posts/summary?start-index=#{luck}&max-results=1&alt=json-in-script")
-        JSON.parse(jsonp[/{.+}/])
-          .dig('feed', 'entry')
-          .first
-          .dig('link')
-          .filter { |link| link['rel'] == 'alternate' }
-          .first
-          .dig('href')
-      end
+      private
 
-      private def get_page_and_parse(url)
-        charset = nil
-        html = URI.parse(url).open do |f|
-          charset = f.charset
-          f.read
-        end
-        ::Nokogiri::HTML.parse(html, nil, charset)
-      end
-
-      private def send_attachement(target:, url:, title:, body:, image_url:)
+      def send_attachement(target:, url:, title:, body:, image_url:)
         attachment = Lita::Adapters::Slack::Attachment.new(body, {
           color: 'good',
           title: title,
@@ -64,6 +44,6 @@ module Lita
       end
     end
 
-    Lita.register_handler(Irastoya)
+    Lita.register_handler(Lita::Handlers::Irastoya)
   end
 end

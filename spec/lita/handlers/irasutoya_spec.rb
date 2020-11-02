655a4fd0b2120749a0071658b46fea641860eba1
Yuji Ueki <unhappychoice@gmail.com>
2020-11-02 11:34:25 +0900

    :green_heart: Update spec

--- spec/lita/handlers/irasutoya_spec.rb
+++ spec/lita/handlers/irasutoya_spec.rb
@@ -14,6 +14,15 @@ RSpec.describe Lita::Handlers::Irasutoya, lita_handler: true do # rubocop:disabl
         ]
       )
     end
+
+    let(:reply_message) do
+      <<~MESSAGE
+        http://www.irasutoya.com/2013/03/blog-post_7115.html
+        幼稚園バス・スクールバスのイラスト
+        かわいい花がらの幼稚園バス（スクールバス）のイラストです。
+        http://1.bp.blogspot.com/-h-jqRxRWp7o/UO6j15wYUUI/AAAAAAAAKmo/KN0WAhYNlDs/s500/youchien_schoolbus.png
+      MESSAGE
+    end
   end
 
   shared_context 'with images' do
@@ -28,58 +37,65 @@ RSpec.describe Lita::Handlers::Irasutoya, lita_handler: true do # rubocop:disabl
         ]
       )
     end
+
+    let(:reply_message) do
+      <<~MESSAGE
+        http://www.irasutoya.com/2013/03/blog-post_7115.html
+        幼稚園バス・スクールバスのイラスト
+        かわいい花がらの幼稚園バス（スクールバス）のイラストです。
+        http://1.bp.blogspot.com/-h-jqRxRWp7o/UO6j15wYUUI/AAAAAAAAKmo/KN0WAhYNlDs/s500/youchien_schoolbus.png
+        http://1.bp.blogspot.com/-h-jqRxRWp7o/UO6j15wYUUI/AAAAAAAAKmo/KN0WAhYNlDs/s500/youchien_schoolbus2.png
+      MESSAGE
+    end
   end
 
-  shared_examples('a command that replies message') { it { is_expected.to include reply_message.strip } }
-
   describe 'Routing' do
-    it { is_expected.to route('irasutoya') }
+    it { is_expected.to route('irasutoya random') }
+    it { is_expected.to route('irasutoya search something') }
   end
 
   describe 'Behavior' do # rubocop:disable Metrics/BlockLength
-    let(:reply_message) { nil }
-
     subject { replies }
 
-    before do
-      allow(Irasutoya::Irasuto).to receive(:random) { irasuto }
-    end
+    shared_examples('a command that replies message') { it { is_expected.to include reply_message.strip } }
 
-    describe 'irasutoya command' do # rubocop:disable Metrics/BlockLength
-      context 'with normal adapters' do # rubocop:disable Metrics/BlockLength
-        before { send_message 'irasutoya' }
+    describe 'random command' do
+      before do
+        allow(Irasutoya::Irasuto).to receive(:random) { irasuto }
+        send_message 'irasutoya random'
+      end
 
+      context 'with normal adapters' do
         context 'with an image' do
           include_context 'with an image'
-
-          let(:reply_message) do
-            <<~MESSAGE
-              http://www.irasutoya.com/2013/03/blog-post_7115.html
-              幼稚園バス・スクールバスのイラスト
-              かわいい花がらの幼稚園バス（スクールバス）のイラストです。
-              http://1.bp.blogspot.com/-h-jqRxRWp7o/UO6j15wYUUI/AAAAAAAAKmo/KN0WAhYNlDs/s500/youchien_schoolbus.png
-            MESSAGE
-          end
-
           it_behaves_like 'a command that replies message'
         end
 
         context 'with multiple images' do
           include_context 'with images'
-
-          let(:reply_message) do
-            <<~MESSAGE
-              http://www.irasutoya.com/2013/03/blog-post_7115.html
-              幼稚園バス・スクールバスのイラスト
-              かわいい花がらの幼稚園バス（スクールバス）のイラストです。
-              http://1.bp.blogspot.com/-h-jqRxRWp7o/UO6j15wYUUI/AAAAAAAAKmo/KN0WAhYNlDs/s500/youchien_schoolbus.png
-              http://1.bp.blogspot.com/-h-jqRxRWp7o/UO6j15wYUUI/AAAAAAAAKmo/KN0WAhYNlDs/s500/youchien_schoolbus2.png
-            MESSAGE
-          end
-
           it_behaves_like 'a command that replies message'
         end
       end
+    end
+
+    describe 'search command' do
+      include_context 'with images'
+
+      let(:irasuto_link) do
+        Irasutoya::IrasutoLink.new(
+          title: '幼稚園バス・スクールバスのイラスト',
+          show_url: 'http://www.irasutoya.com/2013/03/blog-post_7115.html'
+        )
+      end
+
+      before do
+        allow(Irasutoya::Irasuto).to receive(:search) { [irasuto_link] }
+        allow(irasuto_link).to receive(:fetch_irasuto) { irasuto }
+
+        send_message 'irasutoya search irasuto'
+      end
+
+      it_behaves_like 'a command that replies message'
     end
   end
 end
